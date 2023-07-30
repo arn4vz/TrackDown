06d39aff4ebfc4206ed2270385519ff61ac1b05b
Arnav Sharma <128835090+arn4vz@users.noreply.github.com>
2023-07-30 21:26:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://trackd-93798913aa68.herokuapp.com/";
 //TOGGLE for Shorters
 var use1pt=false;
 
